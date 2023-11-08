702730bd988630fe7ccbfb84c3265cb80d20acac
fex <git@fexmail.de>
2023-11-08 19:41:04 +0000

    jenkins ohne cloud nuke install

--- jenkins/test.sh
+++
@@ -1,6 +0,0 @@
-
-for i in $(seq 99); do
-    curl -I http://admin:e17a935058ef4a6ca1cd07aec9a0de45@localhost:8080/job/thesis/buildWithParameters?token=super_secret_token
-    echo "execution $i"
-    sleep 40
-done

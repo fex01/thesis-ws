ab9afc4a2268c98c937bb532f97d0d819854a6af
fex <git@fexmail.de>
2023-11-13 07:38:25 +0000

    option to set build parameters

--- jenkins/trigger_builds.sh
+++ jenkins/trigger_builds.sh
@@ -12,7 +12,29 @@ show_help() {
     echo "  --num-builds       Number of builds to trigger. Default: 10"
     echo "  --username         Jenkins username for authentication."
     echo "  --token            Jenkins API token or user password for authentication."
+    echo "  --parameters       Comma-separated parameters for the build job."
     echo ""
+}
+
+# Function to get valid parameters for a job
+get_valid_parameters() {
+    java -jar jenkins-cli.jar -s "$JENKINS_URL" $AUTH_CMD_PART get-job "$JOB_NAME" | 
+        grep '<name>' | 
+        sed -e 's/<name>\(.*\)<\/name>/\1/' |
+        awk '{$1=$1;print}'  # This awk command trims leading and trailing spaces
+}
+
+# Function to prompt the user to decide on invalid parameters
+prompt_continue_on_invalid_param() {
+    local param_name=$1
+    while true; do
+        read -p "Parameter '$param_name' seems invalid. Do you want to proceed anyway? (y/n) " yn
+        case $yn in
+            [Yy]* ) return 0;;  # User chose to proceed
+            [Nn]* ) return 1;;  # User chose not to proceed
+            * ) echo "Please answer yes (y) or no (n).";;
+        esac
+    done
 }
 
 # Initialize default values
@@ -21,6 +43,7 @@ JOB_NAME="thesis"
 NUM_BUILDS=10
 USERNAME=""
 TOKEN=""
+PARAMETERS=""
 
 # Parse command line arguments
 while [[ "$#" -gt 0 ]]; do
@@ -31,6 +54,7 @@ while [[ "$#" -gt 0 ]]; do
         --num-builds) NUM_BUILDS="$2"; shift; shift ;;
         --username) USERNAME="$2"; shift; shift ;;
         --token) TOKEN="$2"; shift; shift ;;
+        --parameters) PARAMETERS="$2"; shift; shift ;;
         *) echo "Unknown option: $1"; show_help; exit 1 ;;
     esac
 done
@@ -83,12 +107,37 @@ if ! [[ "$NUM_BUILDS" =~ ^[1-9][0-9]*$ ]]; then
     exit 1
 fi
 
+# Parameter validation
+VALID_PARAMETERS=$(get_valid_parameters "$JOB_NAME")
+readarray -t VALID_PARAMS_ARRAY <<< "$VALID_PARAMETERS"
+PARAM_CMD_PART=""
+IFS=',' read -ra PARAM_ARR <<< "$PARAMETERS"
+for param in "${PARAM_ARR[@]}"; do
+    IFS='=' read -ra PARAM_PAIR <<< "$param"
+    PARAM_NAME="${PARAM_PAIR[0]}"
+    PARAM_NAME_FOUND=false
+    for valid_param in "${VALID_PARAMS_ARRAY[@]}"; do
+        if [[ "$valid_param" == "$PARAM_NAME" ]]; then
+            PARAM_NAME_FOUND=true
+            break
+        fi
+    done
+    if [[ "$PARAM_NAME_FOUND" == false ]]; then
+        echo "Warning: Parameter '$PARAM_NAME' may not be valid for job '$JOB_NAME'."
+        if ! prompt_continue_on_invalid_param "$PARAM_NAME"; then
+            echo "Operation aborted by user."
+            exit 1
+        fi
+    fi
+    PARAM_CMD_PART+="-p $param "
+done
+
 for ((i=1; i<=NUM_BUILDS; i++))
 do
-    echo "Starting build $i/$NUM_BUILDS of $JOB_NAME with parameters dynamic_testing=true, nuke=true"
+    echo "Starting build $i/$NUM_BUILDS of $JOB_NAME with parameters: $PARAMETERS"
     
-    # Trigger the build with parameters and wait for its completion
-    java -jar jenkins-cli.jar -s "$JENKINS_URL" $AUTH_CMD_PART build "$JOB_NAME" -f -w -p dynamic_testing=true -p nuke=true
+    # Trigger the build with user-specified parameters
+    java -jar jenkins-cli.jar -s "$JENKINS_URL" $AUTH_CMD_PART build "$JOB_NAME" -f -w $PARAM_CMD_PART
     
     if [[ $? -ne 0 ]]; then
         echo "Error occurred during the triggering of build $i."
